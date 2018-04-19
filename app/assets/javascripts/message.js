036107ad518f8a7bd820c0e68219a73a6efbc19e
nariatsu1202 <atsuki02jojo@gmail.com>
2018-04-19 21:42:41 +0900

    自動で表示される前までの記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,52 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
+  function buildHTML(message){
+    var addImage = '';
+    if (message.image.url) {
+      addImage = `<img src="${message.image.url}" class="lower-message__image">`;
+    }
+    var html = `
+    <div class="chat__contents__content>
+      <div class="chat__contents__content-top>
+        <div class="chat__contents__content-top__user>
+          ${message.user_name}
+        </div>
+        <div class="chat__contents__content-top__timestamp
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="chat__contents__content__text
+        ${message.content}
+        ${addImage}
+      </div>
+    </div>`;
+    return html;
+  }
+
   $('.form-js').on('submit', function(e) {
     e.preventDefault();
 
-    var
+    var formdata = new FormData($(this).get(0));
+
+    $.ajax({
+      url: location.href,
+      type: 'POST',
+      data: formdata,
+      dataType: 'json',
+      contentType: false,
+      processData: false
+    })
+
+    .done(function(message) {
+      console.log('success')
+      console.log(message)
+      var html = buildHTML(message);
+      $('.chat__contents').append(html)
+      $('.form__message').val('');
+      $('.btn').prop('disabled', false);
+    })
+    .fail(function(message) {
+      alert('メッセージを入力してください');
+      console.log('error!');
+    })
   })
 })
