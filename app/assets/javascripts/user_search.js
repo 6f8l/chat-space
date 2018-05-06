cdb1f80e569bec26566686d3eb78203d7b78394e
nariatsu1202 <atsuki02jojo@gmail.com>
2018-05-06 14:21:11 +0900

    インクリメンタルサーチの実装

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,44 +1,39 @@
 $(document).on('turbolinks:load', function() {
-
   var search_list = $(".user-search-result");
 
   function appendUser(user) {
     var html = `<div class='chat-group-user clearfix' id='chat-group-user-${user.id}'>
-          <input name='chat_group[${user.id}][]' type='hidden' value='22'>
-          <p class='chat-group-user__name'>${user.name}</p>
-          <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-          </div>`
+                  <p class='chat-group-user__name'>${user.name}</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
     search_list.append(html);
   };
 
   function appendNoUser(user) {
     var html = ``
-    search_list.appned(html);
+    search_list.append(html);
   };
 
   function buildHTML(id, name) {
     var html = `<div class="chat-group-user clearfix" id=chat-group-user-${id}>
-      <input type="hidden" name="group[user_ids][]" value="${id}">
-      <p class="chat-group-user__name">${name}</p>
-      <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}">削除</a>
-    </div>`
+                  <input type="hidden" name="user_id" value="${id}">
+                  <p class="chat-group-user__name">${name}</p>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}">削除</a>
+                </div>`
     return html
   }
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input);
-    //$.ajax({オプション}).done({処理})
     $.ajax({
       type: 'GET',
       url: '/users',
       data: { name : input },
       dataType: 'json'
     })
-
     .done(function(users) {
-      $("#chat-group-users").empty();
-      if (users.length !== 0) {
+      $(".user-search-result").empty();
+      if (users.length !== 0 && input.length !== 0) {
         users.forEach(function(user) {
           appendUser(user);
         });
@@ -46,23 +41,21 @@ $(document).on('turbolinks:load', function() {
         appendNoUser("一致するユーザーはいません")
       }
     })
-
     .fail(function() {
       alert('ユーザー検索に失敗しました')
     });
   });
 
-  $('.user-search-add').on('click', function() {
-    console.log('hoge');
-    var id = $('user-search-add').data('user-id');
-    var name = $('.user-search-add').data('user-name');
+  $(document).on('click','.user-search-add', function() {
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
     var insertHTML = buildHTML(id, name);
-    $('#chat-group-users').append(insertHTML);
+    $('.chat-group-users').append(insertHTML);
     $(this).parent('.chat-group-user').remove();
   })
 
-  $('#chat-group-users').on('click', '.user-search-remove', function() {
-    var id = $(this).data('id');
+  $(document).on('click', '.user-search-remove', function() {
+    var id = $(this).data('user-id');
     $(`#chat-group-user-${id}`).remove();
   })
 });
